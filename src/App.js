3b4cc183bb2a25bea1f4d9b848d8597fbfd883c8
rajkumar-jeela <rajkumarjeela786@gmail.com>
2022-03-10 09:25:34 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginPage from './components/LoginPage'
+import HomePage from './components/HomePage'
+import ProtectedRoute from './components/ProtectedRoute'
+import MyProfileRoute from './components/MyProfileRoute'
+import NotFound from './components/NotFound'
+import './App.css'
+import UsersProfileRoute from './components/UsersProfileRoute'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginPage} />
+    <ProtectedRoute exact path="/" component={HomePage} />
+    <ProtectedRoute exact path="/my-profile" component={MyProfileRoute} />
+    <ProtectedRoute exact path="/users/:userId" component={UsersProfileRoute} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
